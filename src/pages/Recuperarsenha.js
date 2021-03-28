4cfa85fec5d37d2357aa3b29a7c15875526411fe
Jaqueline Azambuja <jaquelineazambuja.adm@gmail.com>
2021-03-28 17:55:17 -0300

    menu lateral

--- src/pages/Recuperarsenha.js
+++ src/pages/Recuperarsenha.js
@@ -1,122 +1,26 @@
 import "./css/Recuperarsenha.css";
 import React from "react";
+import Sidebar from "../Components/Rodape/Sidebar";
+import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import { withRouter } from "react-router";
+import '../../src/pages/css/Sidebar.css';
+
 
 const Recuperarsenha = () => {
   return (
     <>
-      <div className="barra-lateral">
-        <img
-          src={require("../img/aviaoazul.png").default}
-          id="logo"
-          alt="Imagem responsiva"
-        />
+         <Container fluid>
+                <Row>
+                    <Col xs={2} id="sidebar-wrapper">      
+                      <Sidebar />
+                    </Col>
+                    <Col  xs={10} id="page-content-wrapper">
+                        this is a test
+                    </Col> 
+                </Row>
 
-        <div className="link-to-home">
-          <img
-            src={require("../img/aviaoazul.png").default}
-            id="logo"
-            alt="Imagem responsiva"
-          />
-          <a
-            href="../Login"
-            button
-            type="button"
-            class="btn btn-primary"
-            id="botao1"
-          >
-            {" "}
-            Login
-          </a>
-        </div>
-
-        <div className="link-to-out">
-          <img
-            src={require("../img/aviaoazul.png").default}
-            id="logo"
-            alt="Imagem responsiva"
-          />
-          <a
-            href="../Login"
-            button
-            type="button"
-            class="btn btn-primary"
-            id="botao1"
-          >
-            {" "}
-            Login
-          </a>
-        </div>
-      </div>
-
-      <div className="container-rota">
-        <div className="menu-horizontal">
-          <h1>Traçando rota...</h1>
-          <div className="box-profile">
-            <div className="dados-usuario">
-              <p>Icaro Silva</p>
-              <p>icarosilva@gmail.com</p>
-              <img
-                src={require("../img/aviaoazul.png").default}
-                id="foto-perfil"
-                alt="Imagem responsiva"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="barra-progresso"></div>
-
-        <div className="box-personalizacao">
-          <h2>Que tipo de viagem você gostaria de fazer?</h2>
-
-          <div className="type-travels">
-
-            <div className="item-1">
-              <img
-                src={require("../img/aviaoazul.png").default}
-                id="foto-perfil"
-                alt="Imagem responsiva"
-              />
-            </div>
-
-            <div className="item-2">
-              <img
-                src={require("../img/aviaoazul.png").default}
-                id="foto-perfil"
-                alt="Imagem responsiva"
-              />
-            </div>
-
-            <div className="item-3">
-              <img
-                src={require("../img/aviaoazul.png").default}
-                id="foto-perfil"
-                alt="Imagem responsiva"
-              />
-            </div>
-
-            <div className="item-4">
-              <img
-                src={require("../img/aviaoazul.png").default}
-                id="foto-perfil"
-                alt="Imagem responsiva"
-              />
-            </div>
-          </div>
-        </div>
-
-        <a
-            href="../Login"
-            button
-            type="button"
-            class="btn btn-primary"
-            id="botao1"
-          >
-            {" "}
-            Próximo
-          </a>
-      </div>
-    </>
+            </Container>
+        </>
   );
 };
 
