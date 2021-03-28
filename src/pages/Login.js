11aa841426f6ca309e80d43d2ca4d810647f3faa
Jaqueline Azambuja <jaquelineazambuja.adm@gmail.com>
2021-03-28 14:03:29 -0300

    form login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,48 @@
-import './css/Login.css';
-import React from 'react';
+import "./css/Login.css";
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
 
 
- const Login = () => {
+<div className="flex-container">
+            <div class="rounded-logo"><a ><img src={require('../img/aviaoazul.png').default} id="logo"  alt="Imagem responsiva"  /></a></div>
 
-     return (
-         <div className="container-flex-login">
-            <h1>Pagina de Login</h1>
+            <h1 className="nome">Nome</h1>
+            <div> 
+            <p ClassName="botao">  
+                <a href="../Login" button type="button" class="btn btn-primary" id='botao1'> Login</a>
+                <a href="../Cadastrese"  button type="button" class="btn btn-primary" id='botao2'>Cadastre-se</a>
+           
+ </p>
+         </div>
         </div>
-     );
- }
 
- export default Login;
+        
+        <form action=""  method="post" >
+           <div className="Login-screen-body-item">
+             <div className="Login-app-form">
+               <div className="Login-app-form-group">
+                 <input type="text" name="username" className="Login-app-form-control" placeholder="Usuário" id="username" name="username"></input>
+               </div>
+               <div className="Login-app-form-group message">
+                <input type="password" className="Login-app-form-control" name="senha" placeholder="Senha"  id="senha" name="senha"></input>
+               </div> 
+               
+               <div className="Login-app-form-group buttons">
+                 <button className="Login-app-form-button">APAGAR</button>
+                 <button type="submit" className="Login-app-form-button">ENTRAR</button>
+               </div>
+
+             </div>
+           </div>
+        </form>
+
+       
+       
+    </>
+  );
+};
+
+export default Login;
